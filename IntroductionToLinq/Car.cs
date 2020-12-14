ae25f0a090ddad17e750e2065031f8c5963036d1
Vandana Gupta <vgupta@xpanxion.com>
2020-12-14 19:51:07 +0530

    Second update

---
+++ IntroductionToLinq/Car.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntroductionToLinq
+{
+    public class Car
+    {
+        public int Year { get; set; }
+        public string Manufacturer { get; set; }
+        public string Name { get; set; }
+        public double Displacement { get; set; }
+        public int Cylinders { get; set; }
+        public int City { get; set; }
+        public int Highway { get; set; }
+        public int Combined { get; set; }
+
+        internal static Car ParseFromCsv(string line)
+        {
+            var col = line.Split(",");
+            return new Car
+            {
+                Year = int.Parse(col[0]),
+                Manufacturer = col[1],
+                Name = col[2],
+                Displacement = double.Parse(col[3]),
+                Cylinders = int.Parse(col[4]),
+                City = int.Parse(col[5]),
+                Highway = int.Parse(col[6]),
+                Combined = int.Parse(col[7]),
+
+
+            };
+        }
+    }
+}
+        
--- IntroductionToLinq/Program.cs
+++ IntroductionToLinq/Program.cs
@@ -117,9 +117,274 @@ namespace IntroductionToLinq
             }
 
 
+            Console.WriteLine("------------------------------------------------------------------");
+
+            var cars = ProcessFile("fuel.csv");
+            foreach(var car in cars)
+            {
+                Console.WriteLine(car.Name);
+            }
+            Console.WriteLine("fuel efiicient --------------");
+            var queryCars = cars.OrderByDescending(c => c.Combined)
+                             .ThenBy(c => c.Name);
+            //then by gives the capability of secondary sort
+
+            foreach (var car in queryCars.Take(10))
+            {
+                Console.WriteLine($"{car.Name}-{car.Combined}");
+            }
+
+            Console.WriteLine("--------------");
+            //with query
+            var manufacturers = ProcessManufacturer("manufacturers.csv");
+            var queryCars2 = from car in cars
+                             join manufacturer in manufacturers
+                                on car.Manufacturer equals manufacturer.Name
+                             orderby car.Combined descending, car.Name ascending
+                             select new
+                             {
+                                 manufacturer.Headquarters,
+                                 car.Name,
+                                 car.Combined
+                             };
+            foreach (var car in queryCars2.Take(10))
+            {
+                Console.WriteLine($"{car.Name}-{car.Combined}-{car.Headquarters}");
+            }
+
+            Console.WriteLine("-------------------------------------------------------");
+
+            var queryCars3 =
+                            cars.Join(manufacturers, c => c.Manufacturer, m => m.Name, (c, m) => new
+                            {
+                                Car = c,
+                                Manufacturer = m
+                            })
+                            .OrderByDescending(c => c.Car.Combined)
+                            .ThenBy(c => c.Car.Name)
+                            .Select(c => new
+                            {
+                                c.Manufacturer.Headquarters,
+                                c.Car.Name,
+                                c.Car.Combined
+                            });
+
+            foreach (var car in queryCars3.Take(10))
+            {
+                Console.WriteLine($"{car.Name}-{car.Combined}-{car.Headquarters}");
+            }
+            Console.WriteLine("---------------------------------------------------");
+
+            //to join on two things 
+            //to check the equality both side should have same property like year and Manufacturer
+            var queryCars4 = from car in cars
+                             join manufacturer in manufacturers
+                                on new { car.Manufacturer,car.Year } equals new { Manufacturer= manufacturer.Name,manufacturer.Year }
+                             orderby car.Combined descending, car.Name ascending
+                             select new
+                             {
+                                 manufacturer.Headquarters,
+                                 car.Name,
+                                 car.Combined
+                             };
+            foreach (var car in queryCars4.Take(10))
+            {
+                Console.WriteLine($"{car.Name}-{car.Combined}-{car.Headquarters}");
+            }
+
+            Console.WriteLine("-------------------------------------------------------");
+
+            var queryCars5 =
+                            cars.Join(manufacturers,
+                            c => new { c.Manufacturer, c.Year },
+                            m => new { Manufacturer=m.Name,m.Year }, 
+                            (c, m) => new
+                            {
+                                Car = c,
+                                Manufacturer = m
+                            })
+                            .OrderByDescending(c => c.Car.Combined)
+                            .ThenBy(c => c.Car.Name)
+                            .Select(c => new
+                            {
+                                c.Manufacturer.Headquarters,
+                                c.Car.Name,
+                                c.Car.Combined
+                            });
+
+            foreach (var car in queryCars5.Take(10))
+            {
+                Console.WriteLine($"{car.Name}-{car.Combined}-{car.Headquarters}");
+            }
+            Console.WriteLine("---------------------------------------------------");
+
+
+            var queryCars6 =
+                           from car in cars
+                           group car by car.Manufacturer.ToUpper() into manufacture
+                           orderby manufacture.Key
+                           select manufacture;
+                           
+
+
+            foreach (var group in queryCars6)
+            {
+                Console.WriteLine(group.Key);
+                foreach(var car in group.OrderByDescending(c=>c.Combined).Take(2))
+                {
+                    Console.WriteLine($"\t{car.Name}:{car.Combined}");
+                }
+            }
+
+            Console.WriteLine("---------------------------------------------------");
+
+
+            var queryCars7 =
+                          cars.GroupBy(c => c.Manufacturer.ToUpper())
+                          .OrderBy(g => g.Key);
+
+
+
+
+            foreach (var group in queryCars7)
+            {
+                Console.WriteLine(group.Key);
+                foreach (var car in group.OrderByDescending(c => c.Combined).Take(2))
+                {
+                    Console.WriteLine($"\t{car.Name}:{car.Combined}");
+                }
+            }
+
+
+            Console.WriteLine("-----------------------Group join----------------------------");
+
+
+            var queryCars8 =
+                          from manufacturer in manufacturers
+                          join car in cars on manufacturer.Name equals car.Manufacturer
+                            into carGroup
+                          select new
+                          {
+                              Manufacturer = manufacturer,
+                              Cars = carGroup
+                          };
+
+
+
+
+            foreach (var group in queryCars8)
+            {
+                Console.WriteLine(group.Manufacturer.Name);
+                foreach (var car in group.Cars.OrderByDescending(c => c.Combined).Take(2))
+                {
+                    Console.WriteLine($"\t{car.Name}:{car.Combined}");
+                }
+            }
+
+
+            Console.WriteLine("-----------------------Group join----------------------------");
+
+
+            var queryCars9 =
+                          manufacturers.GroupJoin(cars, m => m.Name, c => c.Manufacturer, (m, g) => new
+                          {
+                              Cars = g,
+                              Manufacturer = m
+                          })
+                          .OrderBy(m => m.Manufacturer.Name);
+
+
+
+
+            foreach (var group in queryCars9)
+            {
+                Console.WriteLine(group.Manufacturer.Name);
+                foreach (var car in group.Cars.OrderByDescending(c => c.Combined).Take(2))
+                {
+                    Console.WriteLine($"\t{car.Name}:{car.Combined}");
+                }
+            }
+
+
+            Console.WriteLine("-----------------------Group join----------------------------");
+
+
+            var queryCars10 =
+                        from car in cars
+                        group car by car.Manufacturer into carGroup
+                        select new
+                        {
+                            Name = carGroup.Key,
+                            Max = carGroup.Max(c => c.Combined),
+                            Min = carGroup.Min(c => c.Combined),
+                            Avg=carGroup.Average(c=>c.Combined)
+                        };
+
+
+            foreach (var result3 in queryCars10)
+            {
+                Console.WriteLine(result3.Name);
+                Console.WriteLine(result3.Max);
+                Console.WriteLine(result3.Min);
+                Console.WriteLine(result3.Avg);
+
+            }
+
+            var top =
+                 cars.Where(c => c.Manufacturer == "BMW" && c.Year == 2016)
+                 .OrderByDescending(c => c.Combined)
+                 .ThenBy(c => c.Name)
+                 .Select(c => c)
+                 .First();
+            Console.WriteLine("-------------");
+            Console.WriteLine(top.Name);
+
+            var top1 =
+               cars
+               .OrderByDescending(c => c.Combined)
+               .ThenBy(c => c.Name)
+               .Select(c => c)
+               .First(c => c.Manufacturer == "BMW" && c.Year == 2016);
+            Console.WriteLine("-------------");
+            Console.WriteLine(top1.Name);
+
+            var result =
+              cars.Any(c => c.Manufacturer == "Ford");
+            Console.WriteLine("-------------");
+            Console.WriteLine(result);
+            var result2 =
+          cars.All (c => c.Manufacturer == "Ford");
+            Console.WriteLine("-------------");
+            Console.WriteLine(result2);
+        
+    }
+
+        private static List<Car> ProcessFile(string path)
+        {
+           return File.ReadAllLines(path)
+                .Skip(1)
+                .Where(line => line.Length >1)
+                .Select(Car.ParseFromCsv)
+                .ToList();
         }
 
-      
+        private static List<Manufacturer> ProcessManufacturer(string path)
+        {
+            var query = File.ReadAllLines(path)
+                        .Where(l => l.Length > 1)
+                        .Select(l =>
+                        {
+                            var col = l.Split(",");
+                            return new Manufacturer
+                            {
+                                Name = col[0],
+                                Headquarters = col[1],
+                                Year = int.Parse(col[2])
+                            };
+                        });
+            return query.ToList();
+        }
+
 
         //Namedmethod
         private static bool NameStartsWithS(Employee arg)
